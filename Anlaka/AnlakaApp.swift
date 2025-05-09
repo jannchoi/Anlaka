a1b056cb5021e7754be0154fa3550c3152205ced
jannchoi <gongjo3695@gmail.com>
2025-05-10 00:00:51 +0900

    git init

---
+++ Anlaka/AnlakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnlakaApp.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AnlakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
