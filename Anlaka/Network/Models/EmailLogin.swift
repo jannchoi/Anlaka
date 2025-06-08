bf0240ef32b5e2a4b422f60efea9d9c9a1f52c4f
jannchoi <gongjo3695@gmail.com>
2025-06-09 00:00:28 +0900

    feat: 로그인 밎 회원가입, 이메일, 애플 로그인 구성

--- Anlaka/Network/Models/EmailLogin.swift
+++ Anlaka/Network/Models/EmailLogin.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+struct EmailLoginRequestDTO: Encodable {
+    let email: String
+    let password: String
+    let deviceToken: String
+}
+
+struct EmailLoginRequestEntity {
+    let email: String
+    let password: String
+    let deviceToken: String
+}
+
+extension EmailLoginRequestEntity {
+    func toDTO() -> EmailLoginRequestDTO {
+        return EmailLoginRequestDTO(
+            email: email,
+            password: password,
+            deviceToken: deviceToken
+        )
+    }
+}
---
+++ Anlaka/Presentation/HomeView/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/15/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
