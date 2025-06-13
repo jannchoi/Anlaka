91d1e208c4ac13d8a0390ef2b63be805e5aee224
jannchoi <gongjo3695@gmail.com>
2025-06-14 00:00:17 +0900

    chore: assetcolor 추가 및customtextfield추가 및 로그인, 회원가입뷰에 적용

--- Anlaka/Presentation/LoginView/LoginView.swift
+++ Anlaka/Presentation/LoginView/LoginView.swift
@@ -19,102 +19,105 @@ struct LoginView: View {
     }
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                Spacer().frame(height: 80)
+            ZStack{
+                Color("WarmLinen")
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer().frame(height: 80)
 
-                Image(systemName: "home")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 200)
-                    .padding()
+                    Text("안락한가")
+                        .font(.largeTitle)
+                        .foregroundStyle(.mainText)
+                        .frame(alignment: .center)
+                        .padding()
 
-                VStack(spacing: 16) {
-                    TextField("Email", text: Binding(
-                        get: { container.model.email },
-                        set: { container.handle(.emailChanged($0)) }
-                    ))
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    VStack(spacing: 16) {
+                        CustomTextField(
+                            title: "이메일",
+                            text: $container.model.email,
+                            keyboardType: .emailAddress,
+                            validationMessage: container.model.emailValidationMessage,
+                            isValid: container.model.isEmailValid
+                        )
+                        .onChange(of: container.model.email) {
+                            container.handle(.emailChanged($0))
+                        }
+                
+                        CustomTextField(
+                            title: "비밀번호",
+                            text: $container.model.password,
+                            isSecure: true,
+                            validationMessage: container.model.passwordValidationMessage,
+                            isValid: container.model.isPasswordValid,
+                            showsToggleVisibilityButton: true
+                        )
+                        .onChange(of: container.model.password) {
+                            container.handle(.passwordChanged($0))
+                        }
 
-
-                    Text(container.model.emailValidationMessage)
-                        .font(.caption)
-                        .foregroundColor(container.model.isEmailValid ? .green : .red)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    
-                    SecureField("Password", text: Binding(
-                        get: { container.model.password },
-                        set: { container.handle(.passwordChanged($0)) }
-                    ))
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
-                    Text(container.model.passwordValidationMessage)
-                        .font(.caption)
-                        .foregroundColor(container.model.isPasswordValid ? .green : .red)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Button {
-                        container.handle(.loginTapped)
-                    } label: {
+                        Button {
+                            container.handle(.loginTapped)
+                        } label: {
+                            if container.model.isLoading {
+                                ProgressView()
+                            } else {
+                                Text("로그인")
+                                    .frame(maxWidth: .infinity)
+                            }
+                        }
+                        .disabled(!container.model.isLoginEnabled)
+                        .frame(height: 50)
+                        .background(Color.oliveMist)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        
                         if container.model.isLoading {
                             ProgressView()
-                        } else {
-                            Text("로그인")
-                                .frame(maxWidth: .infinity)
                         }
-                    }
-                    .disabled(!container.model.isLoginEnabled)
-                    .frame(height: 50)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-                    
-                    if container.model.isLoading {
-                        ProgressView()
-                    }
-                    HStack(spacing: 35) {
-                        Image(systemName: "apple.logo")
-                            .padding(.leading, 47)
-                        Text("Apple로 로그인")
-                            .foregroundStyle(.white)
-                    }
-                    .overlay {
-                        SignInWithAppleButton { request in
-                            request.requestedScopes = [.email, .fullName]
-                        } onCompletion: { result in
-                            container.handle(.handleAppleLogin(result))
+                        HStack(spacing: 35) {
+                            Image(systemName: "apple.logo")
+                                .padding(.leading, 47)
+                            Text("Apple로 로그인")
+                                .foregroundStyle(.white)
                         }
-                        .frame(height: 50)
-                        .frame(maxWidth: .infinity)
-                        .cornerRadius(8)
-                        .blendMode(.overlay)
-                    }
-                    Button {
-                        container.handle(.handleKakaoLogin)
-                    } label: {
-                        Image("kakao_login_button")
-                            .resizable()
-                            .scaledToFit()
+                        .overlay {
+                            SignInWithAppleButton { request in
+                                request.requestedScopes = [.email, .fullName]
+                            } onCompletion: { result in
+                                container.handle(.handleAppleLogin(result))
+                            }
                             .frame(height: 50)
                             .frame(maxWidth: .infinity)
-                    }
-                    .cornerRadius(8)
-                    
+                            .cornerRadius(8)
+                            .blendMode(.overlay)
+                        }
+                        Button {
+                            container.handle(.handleKakaoLogin)
+                        } label: {
+                            Image("kakao_login_button")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 50)
+                                .frame(maxWidth: .infinity)
+                        }
+                        .cornerRadius(8)
+                        
 
-                    Button("Create an account") {
-                        container.handle(.signUpButtontTapped)
-                    }
-                    .foregroundColor(.gray)
+                        Button("Create an account") {
+                            container.handle(.signUpButtontTapped)
+                        }
+                        .foregroundColor(.gray)
 
-                    if let error = container.model.errorMessage {
-                        Text(error)
-                            .foregroundColor(.red)
-                            .font(.footnote)
+                        if let error = container.model.errorMessage {
+                            Text(error)
+                                .foregroundColor(.red)
+                                .font(.footnote)
+                        }
                     }
+                    .padding(.horizontal, 32)
+
+                    Spacer()
                 }
-                .padding(.horizontal, 32)
-
-                Spacer()
             }
             .dismissKeyboardToolbar()
             .onChange(of: container.model.loginCompleted) { completed in
@@ -146,7 +149,6 @@ struct LoginView: View {
             container.model.onLoginSuccess = {
                 if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
                    let window = windowScene.windows.first {
-                    // 루트 뷰를 HomeView로 교체
                     window.rootViewController = UIHostingController(rootView: HomeView())
                     window.makeKeyAndVisible()
                 }
