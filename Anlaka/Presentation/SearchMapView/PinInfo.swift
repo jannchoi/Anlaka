04bca11eae9113a9d6a5f5238da2ff31d1312723
jannchoi <gongjo3695@gmail.com>
2025-06-14 00:00:02 +0900

    refactor: 지도에 매물 pin추가 까지 로직 구성

---
+++ Anlaka/Presentation/SearchMapView/PinInfo.swift
@@ -0,0 +1,13 @@
+//
+//  PinInfo.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/24/25.
+//
+
+import Foundation
+struct PinInfo {
+    let longitude: Double
+    let latitude: Double
+    let title: String
+}
