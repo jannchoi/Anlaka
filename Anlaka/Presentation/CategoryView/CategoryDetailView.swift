eabe260708c754ab1981243196b227cbb5ea34c8
jannchoi <gongjo3695@gmail.com>
2025-06-14 00:00:32 +0900

    feat: Home화면에서의 화면로직 추가

---
+++ Anlaka/Presentation/CategoryView/CategoryDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryDetailView.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/20/25.
+//
+
+import SwiftUI
+
+struct CategoryDetailView: View {
+    let categoryType: String
+    
+    var body: some View {
+        Text("Category Detail View: \(categoryType)")
+    }
+}
