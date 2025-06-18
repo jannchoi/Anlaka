7a3f9be22ce340f6b87a766fb8f9ae99fbb2a105
jannchoi <gongjo3695@gmail.com>
2025-06-19 00:00:27 +0900

    feat: Home화면 DTO,Entity 정의 및 GeoRouter정의

--- Anlaka/Network/Repository/NetworkRepositoryImp.swift
+++ Anlaka/Network/Repository/NetworkRepositoryImp.swift
@@ -8,6 +8,102 @@
 import Foundation
 
 final class NetworkRepositoryImp: NetworkRepository {
+    func getAddressFromGeo(_ geo: GeolocationEntity) async throws -> AddressResponseEntity {
+        let (x,y) = (geo.longitude, geo.latitude)
+        do {
+            let response = try await NetworkManager.shared.callRequest(target: GeoRouter.getAddress(lon: x, lat: y), model: AddressResponseDTO.self)
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    
+    func getDetailEstate(_ estateId: String) async throws -> DetailEstateEntity {
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.detailEstate(estateId: estateId),
+                model: DetailEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    func postLikeEstate(_ estateId: String, _ targetLikeEstate: LikeEstateEntity) async throws -> LikeEstateEntity {
+        let target = targetLikeEstate.toDTO()
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.likeEstate(estateId: estateId, target),
+                model: LikeEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    func getGeoEstate(category: String?, lon: String?, lat: String?, maxD: String?) async throws -> GeoEstateEntity {
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.geoEstate(category: category, lon: lon, lat: lat, maxD: maxD),
+                model: GeoEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    func getTodayEstate() async throws -> TodayEstateEntity {
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.todayEstate,
+                model: TodayEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    func getHotEstate() async throws -> HotEstateEntity {
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.hotEstate,
+                model: HotEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    func getSimilarEstate() async throws -> SimilarEstateEntity {
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.similarEstate,
+                model: SimilarEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
+    func getTopicEstate() async throws -> TopicEstateEntity {
+        do {
+            let response = try await NetworkManager.shared.callRequest(
+                target: EstateRouter.topicEstate,
+                model: TopicEstateResponseDTO.self
+            )
+            return response.toEntity()
+        } catch {
+            throw error
+        }
+    }
+    
     
     func fetchRefreshToken(refToken: String) async throws -> RefreshTokenEntity {
         do {
