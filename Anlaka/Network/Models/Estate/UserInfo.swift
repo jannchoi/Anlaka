d7a8b51fb84996db19d0c801d397488d445ba66a
jannchoi <gongjo3695@gmail.com>
2025-06-24 00:00:33 +0900

    feat: ChatRouter 구현 및 NetworkRepository에 관련 메서드 추가

--- Anlaka/Network/Models/Estate/UserInfo.swift
+++ Anlaka/Network/Models/Estate/UserInfo.swift
@@ -6,6 +6,22 @@
 //
 
 import Foundation
+struct UserInfoResponseDTO: Codable {
+    let userId: String
+    let nick: String
+    let introduction: String?
+    let profileImage: String?
+    
+    func toEntity() -> UserInfoEntity {
+        return UserInfoEntity(
+            userId: userId,
+            nick: nick,
+            introduction: introduction ?? "",
+            profileImage: profileImage ?? ""
+        )
+    }
+}
+
 
 struct UserInfoDTO: Decodable {
     let userId: String?
@@ -40,8 +56,8 @@ extension UserInfoDTO {
         return UserInfoEntity(
             userId: userId,
             nick: nick ?? "알 수 없음",
-            introduction: introduction ?? "알 수 없음",
-            profileImage: profileImage ?? "알 수 없음"
+            introduction: introduction ?? "",
+            profileImage: profileImage ?? ""
         )
     }
 }
