77f4976b3773ec874208198ed61e3753f54b07ce
jannchoi <gongjo3695@gmail.com>
2025-06-24 00:00:13 +0900

    feat: Home화면 UI와 데이터 연결

---
+++ Anlaka/Presentation/Custom/CustomAsyncImage.swift
@@ -0,0 +1,57 @@
+//
+//  CustomAsyncImage.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/20/25.
+//
+
+import SwiftUI
+
+struct CustomAsyncImage: View {
+    let imagePath: String  // 예: "/data/estates/xxx.png"
+    
+    @State private var uiImage: UIImage?
+    @State private var isLoading = true
+    
+    var body: some View {
+        Group {
+            if let uiImage = uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } else if isLoading {
+                Rectangle()
+                    .fill(Color.gray.opacity(0.1))
+                    .overlay(ProgressView())
+            } else {
+                Rectangle()
+                    .fill(Color.gray.opacity(0.3))
+            }
+        }
+        .onAppear {
+            loadImage()
+        }
+    }
+    
+    private func loadImage() {
+        guard let url = URL(string: FormatManager.formatImageURL(imagePath)) else {
+            isLoading = false
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.addValue(Environment.apiKey, forHTTPHeaderField: "SeSACKey")
+        guard let accessToken = UserDefaultsManager.shared.getString(forKey: .accessToken) else {return}
+        request.addValue(accessToken, forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            defer { isLoading = false }
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.uiImage = image
+                }
+            }
+        }.resume()
+    }
+}
+
