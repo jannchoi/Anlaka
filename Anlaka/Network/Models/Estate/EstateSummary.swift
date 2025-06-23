f10279b7bdef827ad20b86a12f7fd205882011cf
jannchoi <gongjo3695@gmail.com>
2025-06-24 00:00:27 +0900

    feat: Home화면 DTO,Entity 정의 및 GeoRouter정의

---
+++ Anlaka/Network/Models/Estate/EstateSummary.swift
@@ -0,0 +1,84 @@
+//
+//  EstateSummary.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/18/25.
+//
+
+import Foundation
+struct EstateSummaryDTO: Decodable {
+    let estateId: String
+    let category: String
+    let title: String
+    let introduction: String
+    let thumbnails: [String]
+    let deposit: Double
+    let monthlyRent: Double
+    let builtYear: String
+    let area: Double
+    let floors: Int
+    let geolocation: GeolocationDTO
+    let distance: Double?
+    let likeCount: Int
+    let isSafeEstate: Bool
+    let isRecommended: Bool
+    let createdAt: String
+    let updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case estateId = "estate_id"
+        case category, title, introduction, thumbnails
+        case deposit, monthlyRent = "monthly_rent"
+        case builtYear = "built_year"
+        case area, floors, geolocation, distance
+        case likeCount = "like_count"
+        case isSafeEstate = "is_safe_estate"
+        case isRecommended = "is_recommended"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
+
+struct EstateSummaryEntity {
+    let estateId: String
+    let category: String
+    let title: String
+    let introduction: String
+    let thumbnails: [String]
+    let deposit: Double
+    let monthlyRent: Double
+    let builtYear: String
+    let area: Double
+    let floors: Int
+    let geolocation: GeolocationEntity
+    let distance: Double?
+    let likeCount: Int
+    let isSafeEstate: Bool
+    let isRecommended: Bool
+    let createdAt: String
+    let updatedAt: String
+}
+
+extension EstateSummaryDTO {
+    func toEntity() -> EstateSummaryEntity {
+        .init(
+            estateId: estateId,
+            category: category,
+            title: title,
+            introduction: introduction,
+            thumbnails: thumbnails,
+            deposit: deposit,
+            monthlyRent: monthlyRent,
+            builtYear: builtYear,
+            area: area,
+            floors: floors,
+            geolocation: geolocation.toEntity(),
+            distance: distance,
+            likeCount: likeCount,
+            isSafeEstate: isSafeEstate,
+            isRecommended: isRecommended,
+            createdAt: createdAt,
+            updatedAt: updatedAt
+        )
+    }
+}
