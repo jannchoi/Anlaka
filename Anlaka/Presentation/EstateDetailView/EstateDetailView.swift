0f653a49f1d62a5fc424d7c08a346a091c3328f5
jannchoi <gongjo3695@gmail.com>
2025-06-29 00:00:32 +0900

    feat: Home화면에서의 화면로직 추가

---
+++ Anlaka/Presentation/EstateDetailView/EstateDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  EstateDetailView.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/20/25.
+//
+
+import SwiftUI
+
+struct EstateDetailView: View {
+    let estateId: String
+    
+    var body: some View {
+        Text("Estate Detail View: \(estateId)")
+    }
+}
