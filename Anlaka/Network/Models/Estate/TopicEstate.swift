87e4c2492b5d6ef605db194ff2e09c16ad0b6284
jannchoi <gongjo3695@gmail.com>
2025-07-09 00:00:54 +0900

    chore: TopicEstate 수정

--- Anlaka/Network/Models/Estate/TopicEstate.swift
+++ Anlaka/Network/Models/Estate/TopicEstate.swift
@@ -6,16 +6,41 @@
 //
 
 import Foundation
-struct TopicEstateResponseDTO: Decodable {
-    let data: [EstateSummaryDTO]
+struct TopicEstateDTO: Decodable {
+    let data: [TopicEstateItemDTO]
 }
 
+struct TopicEstateItemDTO: Decodable {
+    let title: String
+    let content: String
+    let date: String
+    let link: String? // optional
+}
 struct TopicEstateEntity {
-    let data: [EstateSummaryEntity]
+    let items: [TopicEstateItemEntity]
 }
 
-extension TopicEstateResponseDTO {
+struct TopicEstateItemEntity {
+    let title: String
+    let content: String
+    let date: String
+    let link: String
+}
+
+extension TopicEstateDTO {
     func toEntity() -> TopicEstateEntity {
-        .init(data: data.map { $0.toEntity() })
+        let itemEntities = data.map { $0.toEntity() }
+        return TopicEstateEntity(items: itemEntities)
+    }
+}
+
+extension TopicEstateItemDTO {
+    func toEntity() -> TopicEstateItemEntity {
+        return TopicEstateItemEntity(
+            title: title,
+            content: content,
+            date: date,
+            link: link ?? "" // optional → non-optional
+        )
     }
 }
