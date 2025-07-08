088823137df3f359032b1cbd66480f32fff8b0fe
jannchoi <gongjo3695@gmail.com>
2025-07-09 00:00:27 +0900

    feat: Home화면 DTO,Entity 정의 및 GeoRouter정의

---
+++ Anlaka/Network/Models/Geo/Address.swift
@@ -0,0 +1,77 @@
+//
+//  Address.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/18/25.
+//
+
+import Foundation
+struct AddressResponseDTO: Decodable {
+    let meta: MetaDTO
+    let documents: [DocumentDTO]?
+
+    struct MetaDTO: Decodable {
+        let total_count: Int
+    }
+
+    struct DocumentDTO: Decodable {
+        let road_address: RoadAddressDTO?
+        let address: JibunAddressDTO?
+
+        struct RoadAddressDTO: Decodable {
+            let address_name: String?
+            let region_1depth_name: String?
+            let region_2depth_name: String?
+            let region_3depth_name: String?
+            let road_name: String?
+            let underground_yn: String?
+            let main_building_no: String?
+            let sub_building_no: String?
+            let building_name: String?
+            let zone_no: String?
+        }
+
+        struct JibunAddressDTO: Decodable {
+            let address_name: String?
+            let region_1depth_name: String?
+            let region_2depth_name: String?
+            let region_3depth_name: String?
+            let mountain_yn: String?
+            let main_address_no: String?
+            let sub_address_no: String?
+        }
+    }
+}
+
+struct AddressResponseEntity {
+    var roadAddressName: String
+    var roadRegion1: String
+    var roadRegion2: String
+    var roadRegion3: String
+}
+
+extension AddressResponseDTO {
+    func toEntity() -> AddressResponseEntity {
+        guard let document = documents?.first else {
+            return AddressResponseEntity(roadAddressName: "", roadRegion1: "", roadRegion2: "", roadRegion3: "")
+        }
+        
+        if let road = document.road_address {
+            return AddressResponseEntity(
+                roadAddressName: road.address_name ?? "",
+                roadRegion1: road.region_1depth_name ?? "",
+                roadRegion2: road.region_2depth_name ?? "",
+                roadRegion3: road.region_3depth_name ?? ""
+            )
+        } else if let jibun = document.address {
+            return AddressResponseEntity(
+                roadAddressName: jibun.address_name ?? "",
+                roadRegion1: jibun.region_1depth_name ?? "",
+                roadRegion2: jibun.region_2depth_name ?? "",
+                roadRegion3: jibun.region_3depth_name ?? ""
+            )
+        } else {
+            return AddressResponseEntity(roadAddressName: "", roadRegion1: "", roadRegion2: "", roadRegion3: "")
+        }
+    }
+}
--- Anlaka/Presentation/HomeView/HomeView.swift
+++ Anlaka/Presentation/HomeView/HomeView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
+    let di: DIContainer
+    @StateObject private var container: HomeContainer
+    @State private var path = NavigationPath()
+    init(di: DIContainer) {
+        self.di = di
+        _container = StateObject(wrappedValue: di.makeHomeContainer())
+    }
     var body: some View {
         VStack{
             Text("Hello, World!")
@@ -15,4 +22,46 @@ struct HomeView: View {
             print("HomeView")
         }
     }
+    
+}
+
+extension HomeView {
+    @ViewBuilder
+    func renderTodayEstate() -> some View {
+        switch container.model.todayEstate {
+        case .idle, .loading:
+            ProgressView("오늘의 부동산 로딩 중…")
+        case .success(let data):
+            TodayEstateView(entity: data)
+        case .failure(let message):
+            Text("에러: \(message)")
+                .foregroundColor(.red)
+        }
+    }
+    @ViewBuilder
+    func renderHotEstate() -> some View {
+        switch container.model.hotEstate {
+        case .idle, .loading:
+            ProgressView("Hot 매물 로딩 중…")
+        case .success(let data):
+            HotEstateView(entity: data)
+        case .failure(let message):
+            Text("에러: \(message)")
+                .foregroundColor(.red)
+        }
+    }
+    @ViewBuilder
+    func renderTopicEstate() -> some View {
+        switch container.model.topicEstate {
+        case .idle, .loading:
+            ProgressView("Topic 로딩 중…")
+        case .success(let data):
+            TopicEstateView(entity: data)
+        case .failure(let message):
+            Text("에러: \(message)")
+                .foregroundColor(.red)
+        }
+    }
+
+
 }
