f3e64e5a3e6e715675fcaa59ff6307066d160022
jannchoi <gongjo3695@gmail.com>
2025-07-19 01:40:59 +0900

    feat: TabView 추가 및 뷰 연결

---
+++ Anlaka/Presentation/ProfileView/ProfileView.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileView.swift
+//  Anlaka
+//
+//  Created by 최정안 on 6/9/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
