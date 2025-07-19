90e0315f315e09e42a1ce2ebb07bcfd1d7cbe3ac
jannchoi <gongjo3695@gmail.com>
2025-07-19 19:09:04 +0900

    refactor: 채팅방 참여자 정보 DB 로직 수정

--- Anlaka/Domain/Models/ChatMessageEntity.swift
+++ Anlaka/Domain/Models/ChatMessageEntity.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 struct ChatMessageDTO: Codable {
-    let chatID: String
-    let roomID: String
-    let content: String
-    let createdAt: String
-    let updatedAt: String
-    let sender: SenderDTO
+    let chatID: String?
+    let roomID: String?
+    let content: String?
+    let createdAt: String?
+    let updatedAt: String?
+    let sender: String?
     let files: [String]
 
     enum CodingKeys: String, CodingKey {
@@ -20,30 +20,13 @@ struct ChatMessageDTO: Codable {
     }
 }
 
-struct SenderDTO: Codable {
-    let userID: String
-    let nick: String
-    let name: String?
-    let introduction: String?
-    let profileImage: String?
-    let hashTags: [String]?
-
-    enum CodingKeys: String, CodingKey {
-        case userID = "user_id"
-        case nick
-        case name
-        case introduction
-        case profileImage
-        case hashTags
-    }
-}
 struct ChatMessageEntity: Codable {
     let chatID: String
     let roomID: String
     let content: String
     let createdAt: String
     let updatedAt: String
-    let sender: SenderEntity
+    let sender: String
     let files: [String]
 
     enum CodingKeys: String, CodingKey {
@@ -58,53 +41,28 @@ struct ChatMessageEntity: Codable {
     
     var isMine: Bool {
         if let userInfo = UserDefaultsManager.shared.getObject(forKey: .profileData, as: MyProfileInfoEntity.self) {
-            return sender.userID == userInfo.userid
+            return sender == userInfo.userid
         }
         return false
     }
 }
 
-struct SenderEntity: Codable {
-    let userID: String
-    let nick: String
-    let name: String
-    let introduction: String
-    let profileImage: String
-    let hashTags: [String]
-
-    enum CodingKeys: String, CodingKey {
-        case userID = "user_id"
-        case nick
-        case name
-        case introduction
-        case profileImage
-        case hashTags
-    }
-}
-
 extension ChatMessageDTO {
-    func toEntity() -> ChatMessageEntity {
+    func toEntity() -> ChatMessageEntity? {
+        guard let chatID = chatID, let roomID = roomID, let sender = sender else {return nil}
+        
+        // 현재 시간을 ISO8601 형식으로 생성
+        let currentTime = ISO8601DateFormatter().string(from: Date())
+        
         return ChatMessageEntity(
-            chatID: self.chatID,
-            roomID: self.roomID,
-            content: self.content,
-            createdAt: self.createdAt,
-            updatedAt: self.updatedAt,
-            sender: self.sender.toEntity(),
+            chatID: chatID,
+            roomID: roomID,
+            content: content ?? "",
+            createdAt: createdAt ?? currentTime,
+            updatedAt: updatedAt ?? currentTime,
+            sender: sender,
             files: self.files
         )
     }
 }
 
-extension SenderDTO {
-    func toEntity() -> SenderEntity {
-        return SenderEntity(
-            userID: self.userID,
-            nick: self.nick,
-            name: self.name ?? "",
-            introduction: self.introduction ?? "",
-            profileImage: self.profileImage ?? "",
-            hashTags: self.hashTags ?? []
-        )
-    }
-}
