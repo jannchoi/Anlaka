31f8f16e1aa3bd5a82d384ac5e0149e10f536ba4
jannchoi <gongjo3695@gmail.com>
2025-07-24 00:00:51 +0900

    git init

---
+++ Anlaka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anlaka
+//
+//  Created by 최정안 on 5/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
